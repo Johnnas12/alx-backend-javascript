e7ba5b2db0a9366b8566055dc7acd3ac29a3149f
johnnas12 <yohanesmesfin3@gmail.com>
2024-06-26 04:03:17 +0000

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // This task is scoped to the if block
-    let task2 = false; // This task2 is scoped to the if block
+    const task = true; // This task is scoped to the if block
+    const task2 = false; // This task2 is scoped to the if block
   }
 
   return [task, task2];
