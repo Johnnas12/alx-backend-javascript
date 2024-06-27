223f52285960243b34a73fdfa0e5ffbda7c4d270
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-06-27 07:44:20 +0300

    update 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,18 +1,9 @@
 export default function createIteratorObject(report) {
-  let departments = Object.values(report.allEmployees);
-  let employees = [];
-
-  for (let deptEmployees of departments) {
-    employees.push(...deptEmployees);
+    return (function* _() {
+      for (const department of Object.values(report.allEmployees)) {
+        for (const employee of department) {
+          yield employee;
+        }
+      }
+    }());
   }
-
-  let currentIndex = 0;
-
-  return {
-    next() {
-      return currentIndex < employees.length
-        ? employees[currentIndex++]
-        : undefined;
-    },
-  };
-}
