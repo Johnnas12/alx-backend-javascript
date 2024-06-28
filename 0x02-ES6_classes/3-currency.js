ea25c4caad1902910e6212a73bccad9d3cca7af5
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-06-28 19:34:31 +0300

    done

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+    constructor(code, name){
+        this.code = code;
+        this.name = name;
+    }
+
+    get code(){
+        return this._code;
+    }
+
+    set code() {
+        this._code = this.code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = this.name;
+    }
+
+    displayFullCurrency () {
+        return `${this.name} (${this.code})`;
+    }
+
+
+}
