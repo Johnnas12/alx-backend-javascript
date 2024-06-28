f2c3118f9aa273d4b4a422202eaa4c0aaa114b31
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-06-28 13:33:28 +0300

    done

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
-Import {uploadPhoto createUser} from './utils'
+import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser(){
-    let res = {};
+export default async function asyncUploadUser() {
+  let res = {};
 
-    try {
-        const photo  = await uploadPhoto();
-        const user = await createUser();
-        res = { photo, user };
-    } catch (err) {
-        res = {photo: null, user: null};
-    }
-    return res;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
