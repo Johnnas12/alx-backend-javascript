c0ecd48d9d7e12a99a5537f24527616ea335e6f2
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-06-28 19:35:55 +0300

    done

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,30 @@
 export default class Currency {
-    constructor(code, name){
-        this.code = code;
-        this.name = name;
+    constructor(code, name) {
+      this.code = code;
+      this.name = name;
     }
-
-    get code(){
-        return this._code;
+  
+    get code() {
+      return this._code;
     }
-
-    set code() {
-        this._code = this.code;
+  
+    set code(value) {
+      this._code = value;
     }
-
+  
     get name() {
-        return this._name;
+      return this._name;
     }
-
+  
     set name(value) {
-        this._name = this.name;
+      this._name = value;
     }
-
-    displayFullCurrency () {
-        return `${this.name} (${this.code})`;
+  
+    /**
+     * Creates the full string representation of this Currency.
+     * @returns {String}
+     */
+    displayFullCurrency() {
+      return `${this.name} (${this.code})`;
     }
-
-
-}
+  }
