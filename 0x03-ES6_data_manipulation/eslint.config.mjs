6d66702fa3534f4179591194044114d59e2a2d61
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-07-01 22:21:44 +0300

    done

--- 0x03-ES6_data_manipulation/eslint.config.mjs
+++ 0x03-ES6_data_manipulation/eslint.config.mjs
@@ -1,8 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+// eslint.config.js
+import js from "@eslint/js";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    js.configs.recommended,
+
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        }
+    }
 ];
