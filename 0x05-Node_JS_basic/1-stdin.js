e822ca39c91466e7399ec5d9c290fd9a09b1062f
johnnas12 <yohanesmesfin3@gmail.com>
2024-08-21 06:52:17 +0000

    done

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+/**
+ * prompt users to enter their name.
+ * @author Yohanes Mesfin <https://github.com/johnnas12>
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
