b83cb68678c488c85e263b1ec1cc44fae3bb4642
johnnas12 <yohanesmesfin3@gmail.com>
2024-08-21 06:47:23 +0000

    done

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,8 +3,8 @@
  * @param {String} msg The message to display.
  * @author Yohanes Mesfin <https://github.com/johnnas12>
  */
-const displayMessage = (msg) =>{
-    console.log(msg);
+const displayMessage = (msg) => {
+  console.log(msg);
 };
 
 module.exports = displayMessage;
